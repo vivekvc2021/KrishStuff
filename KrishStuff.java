2150e414c90f5154327686b7dedbb9e746e26c00
vivekvc2021 <48871779+vivekvc2021@users.noreply.github.com>
2021-11-01 19:44:31 -0400

    Update KrishStuff.java

--- KrishStuff.java
+++ KrishStuff.java
@@ -13,6 +13,8 @@ public class Krishit {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Krish Stuff Database! Here you can learn all " +
                 "about Krishenraj Ashok, the man-child from San Jose, California!");
-        System.out.println("This database was made by Vivek Chudasama, a ");
+        System.out.println("This database was made by Vivek Chudasama, an exemplary student procrastinating on his" +
+                " computer science midterm :) He made this database as a present for Krish's " +
+                "8th -- sorry, 18th -- birthday. Happy birthday buddy.");
     }
 }
