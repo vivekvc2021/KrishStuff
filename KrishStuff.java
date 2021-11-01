26820e68c26e14204f93d07084bd1d0332675dec
vivekvc2021 <48871779+vivekvc2021@users.noreply.github.com>
2021-11-01 19:48:52 -0400

    Update KrishStuff.java

--- KrishStuff.java
+++ KrishStuff.java
@@ -9,16 +9,22 @@ Srinjoy
  */
 
 public class KrishStuff {
-    private static final String menu = "Which would you like to learn about? \n" +
-            "Harlem \n" + "Srinjoy \n" + "The Bean \n";  // @TODO
+    private static final String menu = "Krish Stuff Database \n" +
+            "Which would you like to learn about? \n" +
+            "Harlem \n" +
+            "Srinjoy \n" +
+            "The Bean \n";  // @TODO
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Krish Stuff Database! Here you can learn all " +
+        System.out.println("Welcome to the Krish Shit Database! Here you can learn all " +
                 "about Krishenraj Ashok, the man-child from San Jose, California!");
         System.out.println("This database was made by Vivek Chudasama, an exemplary student procrastinating on his" +
                 " computer science midterm :) He made this database as a present for Krish's " +
                 "8th -- sorry, 18th -- birthday. Happy birthday buddy.");
-        System.out.println(menu);
+        boolean keepGoing = true;
+        while (keepGoing) {
+            System.out.println(menu);
+        }
     }
 }
