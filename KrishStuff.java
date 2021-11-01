26e365c2d41b1c5f2cb4e7f4b62a5d794fe04e75
vivekvc2021 <48871779+vivekvc2021@users.noreply.github.com>
2021-11-01 19:54:10 -0400

    Update KrishStuff.java

--- KrishStuff.java
+++ KrishStuff.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 /*
-Krish Stuff:
+Krish Shit:
 Harlem
 The Bean
 Srinjoy
 
  */
 
-public class KrishStuff {
-    private static final String menu = "Krish Stuff Database \n" +
-            "Which would you like to learn about? \n" +
-            "Harlem \n" +
-            "Srinjoy \n" +
-            "The Bean \n";  // @TODO
+public class Krishit {
+    private static final String menu = "Krish Shit Database \n" +
+            "Which would you like to learn about? Please type in the corresponding number \n" +
+            "1. Harlem \n" +
+            "2. Srinjoy \n" +
+            "3. The Bean \n" +
+            "4. [Quit]";  // @TODO
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -25,6 +26,27 @@ public class KrishStuff {
         boolean keepGoing = true;
         while (keepGoing) {
             System.out.println(menu);
+            String answer = scanner.nextLine();
+            if (answer.equals("4")) {
+                keepGoing = false;
+                break;
+            } else {
+                System.out.println(response(answer));
+            }
+        }
+        System.out.println("Thank you for visiting the Krishit Database! Come back anytime!");
+    }
+
+    public static String response(String answer) {
+        switch(answer) {
+            case "1":
+                return "Harlem is a place where our Krish likes to pretend he grew up! Of course," +
+                        " in reality he is from San Jose, CA (but likes to pretend he's from Cupertino) " +
+                        ".";
+            case 2:
+                return "Srinjoy is possibly the love of Krish's life. Krish finds ways to bring Srinjoy " +
+                        "into every conversation, regardless of whether Srinjoy is relevant in the conversation." +
+                        " If you would like to visit Srinjoy, you can find him in the McCutcheon Hall.";
         }
     }
 }
