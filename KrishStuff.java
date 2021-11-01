fe04a1e66190262cdb367f6ae1b29f5c2b52073d
vivekvc2021 <48871779+vivekvc2021@users.noreply.github.com>
2021-11-01 19:43:15 -0400

    Update KrishStuff.java

--- KrishStuff.java
+++ KrishStuff.java
@@ -1,7 +1,18 @@
 import java.util.*;
 
+/*
+Krish Stuff:
+Harlem
+The Bean
+Srinjoy
+
+ */
+
 public class Krishit {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Krish Stuff Database! Here you can learn all " +
+                "about Krishenraj Ashok, the man-child from San Jose, California!");
+        System.out.println("This database was made by Vivek Chudasama, a ");
     }
 }
