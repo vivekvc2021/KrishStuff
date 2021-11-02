6e55e57b55f2804627a204910f484e125126f155
vivekvc2021 <48871779+vivekvc2021@users.noreply.github.com>
2021-11-01 21:37:10 -0400

    Update KrishStuff.java

--- KrishStuff.java
+++ KrishStuff.java
@@ -12,12 +12,12 @@ Super Mario Galaxy Scandal
 
 public class KrishStuff {
     private static final String menu = "Krish Stuff Database \n" +
-            "Which would you like to learn about? Please type in the corresponding number \n" +
-            "1. Harlem \n" +
-            "2. Srinjoy \n" +
-            "3. The Bean \n" +
-            "4. Short \n" +
-            "5. Super Mario Galaxy Scandal \n" +
+            "Which would you like to learn about? Please type in the corresponding number.\n" +
+            "1. The Tale of the Homeland\n" +
+            "2. The Love Story\n" +
+            "3. Chicago's Famous Sculpture\n" +
+            "4. The Height Story\n" +
+            "5. The Scandal in the Galaxy of Super Mario\n" +
             "6. [Quit]";  // @TODO
 
     public static void main(String[] args) throws InterruptedException {
@@ -31,6 +31,7 @@ public class KrishStuff {
         Thread.sleep(3000);
         boolean keepGoing = true;
         while (keepGoing) {
+            System.out.println();
             System.out.println(menu);
             String answer = scanner.nextLine();
             if (answer.equals("4")) {
@@ -38,31 +39,33 @@ public class KrishStuff {
                 break;
             } else {
                 System.out.println(response(answer));
+                Thread.sleep(3000);
             }
         }
-        System.out.println("Thank you for visiting the Krishit Database! Come back anytime!");
+        System.out.println("Thank you for visiting the Krish Stuff Database! Come back anytime!");
     }
 
     public static String response(String answer) {
         switch(answer) {
             case "1":
-                return "Harlem\n" + "Harlem is a place where our Krish likes to pretend he grew up! Of course," +
+                return "The Tale of the Homeland\n" +
+                        "Harlem is a place where our Krish likes to pretend he grew up! Of course," +
                         " in reality he is from San Jose, CA (but likes to pretend he's from Cupertino) " +
                         ".";
             case "2":
-                return "Srinjoy\n" + "Srinjoy is possibly the love of Krish's life. Krish finds ways to bring Srinjoy " +
+                return "The Love Story\n" + "Srinjoy is possibly the love of Krish's life. Krish finds ways to bring Srinjoy " +
                         "into every conversation, regardless of whether Srinjoy is relevant in the conversation." +
                         " If you would like to visit Srinjoy, you can find him in the McCutcheon Hall.";
             case "3":
-                return "The Bean\n" + "is a structure in Chicago, Illinois, built by architect and " +
-                        "VantaBlack creator Anish Kapoor (whom we colloquiolly refer to as Rich Anish). " +
+                return "Chicago's Famous Sculpture\n" + "The Bean is a structure in Chicago, Illinois, built by architect and " +
+                        "VantaBlack creator Anish Kapoor (whom we colloquiolly refer to as VantaBlack Anish). " +
                         "For some reason, Krish is under the impression that The Bean is the coolest place in existence" + "" +
                         "despite how overrated it is. Weirdly, Krish has not talked about the Bean as much " +
                         "after visiting the Bean, a phenomenon humanity attributes to the overrated-ness of the Bean.";
             case "4":
-                return "Short\n" + "Krish is short.";
+                return "The Height Story\n" + "Krish is short.";
             case "5":
-                return "Super Mario Galaxy Scandal\n" + "Krish coerced Srinjoy (for information on Srinjoy, " +
+                return "The Scandal in the Galaxy of Super Mario\n" + "Krish coerced Srinjoy (for information on Srinjoy, " +
                         "type '2') into breaking the law by selling a faulty game to GameStop. Krish " +
                         "over-dramatasizes this by referring to it as the 'Super Mario Galaxy Scandal', when in" +
                         "reality it is quite civil and not at all a scandal.";
